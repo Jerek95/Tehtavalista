1cbb889701abe12eefdeb7b0987e47b3542fd6d9
Jere Koivuranta <jere.koivuranta@gmail.com>
2025-10-30 16:20:45 +0200

    osat 1,2 ja 3 tehty

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    setTasks([...tasks,task]);
+    setTask('');
+  }
+
+  const deleteTask = (deleted) => {
+    const withoutRemoved = tasks.filter(item => item!==deleted)
+    setTasks(withoutRemoved)
+  }
 
   return (
       <div id='container'>
         <h1>Tehtävälista</h1>
         <form>
-          <input placeholder='Syötä uusi tehtävä' />
+          <input 
+          placeholder='Syötä uusi tehtävä' 
+          value = {task}
+          onChange={e => setTask(e.target.value)}
+          onKeyDown={e => {
+            if (e.key === 'Enter') {
+              e.preventDefault()
+              addTask()
+            }
+          }}
+          />
         </form>
-        <ul></ul>
+        <ul>
+          {
+            tasks.map(item => (
+            <li>
+              {item}
+              <button
+                className='delete-button'
+                onClick={() => deleteTask(item)}>
+                Delete
+              </button>
+            </li>
+            ))
+          }
+        </ul>
       </div>
 
   )
