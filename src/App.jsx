7d429b0fbac8b50fc8ce617907154f93196746c7
Jere Koivuranta <jere.koivuranta@gmail.com>
2025-11-04 15:57:49 +0200

    osa 7 tehty

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios'
 import Row from './components/Row'
+import { useUser } from '../context/useUser'
 
 const url = "http://localhost:3001"
 
 function App() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
+  const { user } = useUser()
   
 
   useEffect(() => {
@@ -21,9 +23,10 @@ function App() {
   }, [])
 
   const addTask = () => {
+    const headers = {headers: {Authorization: user.token}}
     const newTask = { description: task }
 
-    axios.post(url + "/create", {task: newTask})
+    axios.post(url + "/create", {task: newTask}, headers)
       .then(response => {
         setTasks([...tasks,response.data])
         setTask('')
@@ -35,7 +38,9 @@ function App() {
 
 
   const deleteTask = (deleted) => {
-    axios.delete(url + "/delete/" + deleted)
+    const headers = {headers: {Authorization: user.token}}
+
+    axios.delete(url + "/delete/" + deleted, headers)
       .then(response => {
         setTasks(tasks.filter(item => item.id !== deleted))
       })
